98877cc46d23bd7e57654c498ca8ea77a005ad14
Jan-Peter Voigt <jpv@schoene-noten.de>
2017-05-03 20:37:47 +0200

    api.scm

---
+++ api.scm
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+% lilypond-export                                                             %
+% ===============                                                             %
+%                                                                             %
+% export foreign file formats with LilyPond                                   %
+%                                                                             %
+% lilypond-export is free software: you can redistribute it and/or modify     %
+% it under the terms of the GNU General Public License as published by        %
+% the Free Software Foundation, either version 3 of the License, or           %
+% (at your option) any later version.                                         %
+%                                                                             %
+% lilypond-export is distributed in the hope that it will be useful,          %
+% but WITHOUT ANY WARRANTY; without even the implied warranty of              %
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               %
+% GNU General Public License for more details.                                %
+%                                                                             %
+% You should have received a copy of the GNU General Public License           %
+% along with openLilyLib. If not, see <http://www.gnu.org/licenses/>.         %
+%                                                                             %
+% lilypond-export is maintained by Jan-Peter Voigt, jp.voigt@gmx.de           %
+%                                                                             %
+%       Copyright Jan-Peter Voigt, 2017                                       %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+(define-module (lilypond-export api))
+
