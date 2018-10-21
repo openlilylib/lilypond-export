e6c6541127a473a6b93f2d7595f85f8bdcc65db0
Jan-Peter Voigt <jpv@schoene-noten.de>
2018-10-21 14:52:22 +0200

    MusicXML write dots before accidental

--- MusicXML.scm
+++ MusicXML.scm
@@ -147,9 +147,9 @@
 
            (writeln "<voice>~A</voice>" voice)
            (writetype dur)
+           (writedots (if (ly:duration? dur) (ly:duration-dot-count dur) 0))
            (if accidental
                (writeln "<accidental>~A</accidental>" (acctext accidental)))
-           (writedots (if (ly:duration? dur) (ly:duration-dot-count dur) 0))
 
            (if (symbol? beam) (writeln "<beam number=\"1\">~A</beam>" beam))
            (writetimemod dur)
